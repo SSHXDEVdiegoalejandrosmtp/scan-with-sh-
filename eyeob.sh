4b0112dd79508c2fef0c3e2eea78ebf724289903
SSHXDEVdiegoalejandrosmtp <sshxdevdiegoalejandrogo@gmail.com>
2024-05-08 17:42:58 -0600

    Add files via upload

---
+++ eyeob.sh
@@ -0,0 +1,61 @@
+echo " * iniciando escaneo de red  automatizado  anada her ip in the  line 5 from  the code "
+echo "diego alejandro chavez  "
+
+read -p "Press [Enter] key to start backup..."
+
+## Bash add pause prompt for 5 seconds ##
+read -t 1  -p " key djfkfrmklflktgd2324 arris       "
+
+echo "  installing tools necesary use python  3 2 work  today $ make  "
+
+
+clear
+
+echo "updating the system     c:    "
+
+apt update -y   
+
+apt install -y python3
+
+
+
+
+
+
+
+function pause(){
+   read -p "$*"
+}
+ 
+# ...
+# call it
+pause 'Press [Enter] key to continua     credits diegoalejandrochavez   ...' 
+# rest of the scrip
+
+clear 
+
+
+echo Please enter your IP/domain address for tools  vulnerability scanning:
+read IP 
+
+echo Your results against $ip will be output in sometime
+
+nmap -sP  -oG output.txt $ip | grep open
+
+
+
+
+
+
+exit
+
+
+
+# errores 
+
+else 
+echo " know error  "
+
+
+
+
